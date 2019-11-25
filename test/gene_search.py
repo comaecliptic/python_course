58b9a0fc4ada182286516b8ed729568dd583e8cf
Shamil Urazbakhtin <shamil.utaraptor@gmail.com>
2019-11-26 00:59:57 +0300

    Add reverse complement to gene search.

--- test/gene_search.py
+++ test/gene_search.py
@@ -1,3 +1,13 @@
+complement_table = str.maketrans('atgc', 'TACG')
+
+
+def reverse_complement(seq):
+    """Return reverse complement of DNA sequence in a fancy way.
+    Do not raise exception if the sequence has nonDNA characters.
+    """
+    return seq[::-1].lower().translate(complement_table)
+
+
 def gene_search(dna_seq):
     """Search DNA sequence for protein-coding
     open reading frames. ORF has following properties:
@@ -15,21 +25,40 @@ def gene_search(dna_seq):
     genes : list of str
         List with all ORFs.
     """
+    def gene_search_core(dna_seq, length, genes):
+        """Core part of gene search. Walk through
+        sequence, parse for genes, append to list.
+
+        Parameters
+        ----------
+        dna_seq : str
+            Input DNA sequence.
+        length : int
+            Length of DNA sequence.
+        genes : list of str
+            List with genes found.
+        """
+        i = 0
+        while i < length:
+            if dna_seq[i:i + 3] == 'ATG':
+                for j in range(i, length, 3):
+                    if dna_seq[j:j + 3] in {'TAA', 'TAG', 'TGA'}:
+                        if len(dna_seq[i:j + 3]) > 12:
+                            genes.append(dna_seq[i:j + 3])
+                        else:
+                            break
+            i += 1
+
     dna_seq = dna_seq.upper()
-    i = 0
     genes = []
     length = len(dna_seq)
-    while i < length:
-        if dna_seq[i:i + 3] == 'ATG':
-            for j in range(i, length, 3):
-                if dna_seq[j:j + 3] in {'TAA', 'TAG', 'TGA'}:
-                    if len(dna_seq[i:j+3]) > 12:
-                        genes.append(dna_seq[i:j+3])
-                    else:
-                        break
-        i += 1
+    reverse_seq = reverse_complement(dna_seq)
+    for seq in {dna_seq, reverse_seq}:
+        gene_search_core(seq, length, genes)
     return genes
 
 
 dna = 'atggtaatgtaacaacaacaaccgccgcactgatgtatgggaggagcaactgaatagctg'
 assert gene_search(dna) == ['ATGTATGGGAGGAGCAACTGA', 'ATGGGAGGAGCAACTGAATAG']
+reverse_dna = reverse_complement(dna)
+assert gene_search(dna) == gene_search(reverse_dna)
